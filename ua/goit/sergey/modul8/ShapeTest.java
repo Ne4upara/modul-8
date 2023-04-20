8e309ad0eaaa5bb856a5b32abfd7a0d7bf42a6cc
Ne4upara <127094971+Ne4upara@users.noreply.github.com>
2023-04-20 21:48:55 +0300

    Add files via upload

--- ua/goit/sergey/modul8/ShapeTest.java
+++ ua/goit/sergey/modul8/ShapeTest.java
@@ -1,57 +1,57 @@
 package ua.goit.sergey.modul8;
 
-//public class ShapeTest {
+public class ShapeTest {
+
+    public static void main(String[] args) {
+
+        Shape rectangle = new Rectangle("Прямоугольник", 3.0, 4.0);
+        Shape quad = new Quad("Квадрат", 2.0);
+        Shape circle = new Circle("Круг", 4);
+        Shape trapezoid = new Trapezoid("Трапеция", 3, 6, 4);
+        EquilateralTtriangle triangle = new Triangle("Треугольник", 5, 5,5 );
+        Shape ellipse = new Ellipse("Элипс", 10, 6);
+        EquilateralTtriangle equilateralTtriangle = new EquilateralTtriangle("Tреугольник равностороний", 5);
+        EquilateralTtriangle isoscelesTriangle = new IsoscelesTriangle("Треугольник равнобедренный", 5 , 5);
+
+        ShowName showName = new ShowName();
+
+        showName.showName(rectangle);
+        showName.showName(quad);
+        showName.showName(circle);
+        showName.showName(trapezoid);
+        showName.showName(triangle);
+        showName.showName(ellipse);
+        showName.showName(equilateralTtriangle);
+        showName.showName(isoscelesTriangle);
+
+
+//        printer.printName(quad);
+//       quad.getArea();
+//        quad.getPerimeter();
 //
-//    public static void main(String[] args) {
+//        rectangle.getArea();
+//        rectangle.getPerimeter();
 //
-//        Shape rectangle = new Rectangle("Прямоугольник", 3.0, 4.0);
-//        Shape quad = new Quad("Квадрат", 2.0);
-//        Shape circle = new Circle("Круг", 4);
-//        Shape trapezoid = new Trapezoid("Трапеция", 3, 6, 4);
-//        EquilateralTtriangle triangle = new Triangle("Треугольник", 5, 5,5 );
-//        Shape ellipse = new Ellipse("Элипс", 10, 6);
-//        EquilateralTtriangle equilateralTtriangle = new EquilateralTtriangle("Tреугольник равностороний", 5);
-//        EquilateralTtriangle isoscelesTriangle = new IsoscelesTriangle("Треугольник равнобедренный", 5 , 5);
+//        circle.getArea();
+//        circle.getPerimeter();
 //
-//        ShowName showName = new ShowName();
+//        ellipse.getArea();
+//        ellipse.getPerimeter();
 //
-//        showName.showName(rectangle);
-//        showName.showName(quad);
-//        showName.showName(circle);
-//        showName.showName(trapezoid);
-//        showName.showName(triangle);
-//        showName.showName(ellipse);
-//        showName.showName(equilateralTtriangle);
-//        showName.showName(isoscelesTriangle);
+//        trapezoid.getArea();
+//        trapezoid.getPerimeter();
 //
+//        equilateralTtriangle.getMedian();
+//        equilateralTtriangle.getArea();
+//        equilateralTtriangle.getPerimeter();
 //
-////        printer.printName(quad);
-////        quad.getArea();
-////        quad.getPerimeter();
-////
-////        rectangle.getArea();
-////        rectangle.getPerimeter();
-////
-////        circle.getArea();
-////        circle.getPerimeter();
-////
-////        ellipse.getArea();
-////        ellipse.getPerimeter();
-////
-////        trapezoid.getArea();
-////        trapezoid.getPerimeter();
-////
-////        equilateralTtriangle.getMedian();
-////        equilateralTtriangle.getArea();
-////        equilateralTtriangle.getPerimeter();
-////
-////        isoscelesTriangle.getMedian();
-////        isoscelesTriangle.getArea();
-////        isoscelesTriangle.getPerimeter();
-////
-////        triangle.getMedian();
-////        triangle.getArea();
-////        triangle.getPerimeter();
+//        isoscelesTriangle.getMedian();
+//        isoscelesTriangle.getArea();
+//        isoscelesTriangle.getPerimeter();
 //
-//    }
-//}
+//        triangle.getMedian();
+//        triangle.getArea();
+//        triangle.getPerimeter();
+
+    }
+}
